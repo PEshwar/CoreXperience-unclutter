ef553a94a6428e35de36aa179d1cf7b2088b1279
Prabhu Eshwarla <prabhueshwarla@Prabhus-MacBook-Pro.local>
2014-10-12 22:10:54 +0530

    Initial Commit

---
+++ CoreXperience/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreXperience
+//
+//  Created by Prabhu Eshwarla on 12/10/14.
+//  Copyright (c) 2014 iTripuram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
